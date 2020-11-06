03a6074446292fe06d363c91fda28d382d6fe9ee
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-11-06 23:40:26 +0100

    "MniamPersonalization" solution was added. "Personalizator" C++ project was added. The "main" function from "Personalizator.cpp" uses the "sum" function declared in "sum.h" and defined in "sum.cpp".

---
+++ MniamPersonalization/Personalizator/Personalizator.cpp
@@ -0,0 +1,14 @@
+// Personalizator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "sum.h"
+
+/**
+ * @brief The main function of the project
+ * @return An integer which can be 0 if the execution was successful or a value different than 0 otherwise
+*/
+int main()
+{
+    std::cout << "The sum of 2 and 5 is: " << sum(2, 5) << '\n';
+}
---
+++ MniamPersonalization/Personalizator/sum.cpp
@@ -0,0 +1,12 @@
+#include "sum.h"
+
+/**
+ * @brief Function which calculates the sum of 2 integers.
+ * @param a The first integer to be added
+ * @param b The second integer to be added
+ * @return The sum of the two given integers
+*/
+int sum(int a, int b)
+{
+    return a + b;
+}
