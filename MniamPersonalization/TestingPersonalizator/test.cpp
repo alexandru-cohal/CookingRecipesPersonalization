e06a557324502dbd5505cc93f89633292e9326bd
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-11-18 18:00:49 +0100

    The initial folder structure of the "Personalizator" project is defined. The testing source and header files were removed.

--- MniamPersonalization/TestingPersonalizator/test.cpp
+++ MniamPersonalization/TestingPersonalizator/test.cpp
@@ -1,14 +1,1 @@
 #include "pch.h"
-#include "../Personalizator/sum.h"
-
-TEST(SumTest, PositiveSum) {
-	ASSERT_EQ(sum(1, 5), 6);
-}
-
-TEST(SumTest, NegativeSum) {
-	ASSERT_EQ(sum(-6, 2), -4);
-}
-
-TEST(SumTest, ZeroSum) {
-	ASSERT_EQ(sum(-2, 1), 0);
-}
